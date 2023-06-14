011b585ff00d9e49ccf535254640cd1cb5faae02
GEOPREDICT <geos@GEOPREDICTs-MacBook-Air.local>
2023-06-14 09:34:39 -0700

    Added files

---
+++ app.py
@@ -0,0 +1,17 @@
+import pickle
+import string
+import streamlit as st
+import webbrowser
+
+global Lrdetect_Model
+
+LrdetectFile = open('model.pckl','rb')
+Lrdetect_Model = pickle.load(LrdetectFile)
+LrdetectFile.close()
+st.title("MY LANGUAGE")
+input_test = st.text_input("provide your text input here", 'Hello I am Masroor')
+
+
+button_clicked = st.button("Click to get Language")
+if button_clicked:
+	st.text(Lrdetect_Model.predict([input_test]))
